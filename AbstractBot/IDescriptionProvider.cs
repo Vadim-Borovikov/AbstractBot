9d83cd49336534bead55df755c3d23787ac7cfcc
Vadim Borovikov <vadim.borovikov@gmail.com>
2021-02-06 12:13:42 +0300

    ReSharper messages suppressing

--- AbstractBot/IDescriptionProvider.cs
+++ AbstractBot/IDescriptionProvider.cs
@@ -1,5 +1,9 @@
-﻿namespace AbstractBot
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AbstractBot
 {
+    [SuppressMessage("ReSharper", "MemberCanBeInternal")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public interface IDescriptionProvider
     {
         string GetDescription();
