370d99982a7c1b8a2da9a6ef35d87182e4eff4b9
Vadim Borovikov <vadim.borovikov@gmail.com>
2021-02-08 22:55:02 +0300

    FinalizeStatusMessageAsync added

---
+++ AbstractBot/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace AbstractBot
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static class Utils
+    {
+        public static Task<Message> FinalizeStatusMessageAsync(this ITelegramBotClient client, Message message,
+            string postfix = "")
+        {
+            string text = $"_{message.Text}_ Готово.{postfix}";
+            return client.EditMessageTextAsync(message.Chat, message.MessageId, text, ParseMode.Markdown);
+        }
+    }
+}
