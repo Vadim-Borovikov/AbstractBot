6096a68b9eda94dc2d8025ea686f9bceb2cabf0b
Vadim Borovikov <vadim.borovikov@gmail.com>
2021-12-02 19:42:34 +0300

    Added EscapeCharacters for MarkdownV2

--- AbstractBot/Utils.cs
+++ AbstractBot/Utils.cs
@@ -12,8 +12,30 @@ namespace AbstractBot
         public static Task<Message> FinalizeStatusMessageAsync(this ITelegramBotClient client, Message message,
             string postfix = "")
         {
-            string text = $"_{message.Text}_ Готово.{postfix}";
-            return client.EditMessageTextAsync(message.Chat, message.MessageId, text, ParseMode.Markdown);
+            string text = $"_{message.Text}_ Готово\\.{postfix}";
+            return client.EditMessageTextAsync(message.Chat, message.MessageId, text, ParseMode.MarkdownV2);
+        }
+
+        public static string EscapeCharacters(string s)
+        {
+            return s.Replace("_", "\\_")
+                    .Replace("*", "\\*")
+                    .Replace("[", "\\[")
+                    .Replace("]", "\\]")
+                    .Replace("(", "\\(")
+                    .Replace(")", "\\)")
+                    .Replace("~", "\\~")
+                    .Replace("`", "\\`")
+                    .Replace(">", "\\>")
+                    .Replace("#", "\\#")
+                    .Replace("+", "\\+")
+                    .Replace("-", "\\-")
+                    .Replace("=", "\\=")
+                    .Replace("|", "\\|")
+                    .Replace("{", "\\{")
+                    .Replace("}", "\\}")
+                    .Replace(".", "\\.")
+                    .Replace("!", "\\!");
         }
     }
 }
