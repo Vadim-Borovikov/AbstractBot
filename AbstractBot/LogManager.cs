66daaade636aa823349c7e3ff80760a9abc9a02f
Вадим Боровиков <vadim.borovikov@gmail.com>
2022-06-28 16:01:54 +0400

    Old logs deletion

--- AbstractBot/LogManager.cs
+++ AbstractBot/LogManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 
@@ -8,14 +10,22 @@ namespace AbstractBot;
 [PublicAPI]
 public sealed class LogManager
 {
-    internal LogManager() => _timeManager = new TimeManager();
+    internal LogManager()
+    {
+        _timeManager = new TimeManager();
+        if (!Directory.Exists(MessagesLogDirectory))
+        {
+            Directory.CreateDirectory(MessagesLogDirectory);
+        }
+        DeleteOldLogs();
+    }
 
     public void SetTimeZone(string? timeZoneId = null) => _timeManager = new TimeManager(timeZoneId);
 
     public void LogMessage(string? message = null)
     {
-        DateTime today = _timeManager.Now().Date;
-        string path = string.Format(MessagesLogPathTemplete, $"{today:dd.MM.yyyy}");
+        string name = GetLogNameFor(_timeManager.Now().Date);
+        string path = Path.Combine(MessagesLogDirectory, name);
         InsertToStart(path, $"{message}{Environment.NewLine}");
     }
 
@@ -51,8 +61,30 @@ public sealed class LogManager
         File.WriteAllText(path, $"{contents}{text}");
     }
 
+    private void DeleteOldLogs()
+    {
+        HashSet<string> newLogs = new();
+        for (byte days = 0; days < LogsToHold; ++days)
+        {
+            DateTime date = _timeManager.Now().Date.AddDays(-days);
+            string name = GetLogNameFor(date);
+            newLogs.Add(name);
+        }
+
+        List<string> oldLogs =
+            Directory.EnumerateFiles(MessagesLogDirectory).Where(f => !newLogs.Contains(f)).ToList();
+        foreach (string log in oldLogs)
+        {
+            File.Delete(log);
+        }
+    }
+
+    private static string GetLogNameFor(DateTime day) => string.Format(MessagesLogPathTemplete, $"{day:dd.MM.yyyy}");
+
     private TimeManager _timeManager;
 
     private const string ExceptionsLogPath = "errors.txt";
+    private const string MessagesLogDirectory = "Logs";
     private const string MessagesLogPathTemplete = "log {0}.txt";
+    private const byte LogsToHold = 5;
 }
