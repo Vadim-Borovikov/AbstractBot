53d6d1d7ee64d59b093c88f5ecfea90f6e98f12e
Вадим Боровиков <vadim.borovikov@gmail.com>
2022-12-03 21:01:21 +0200

    StatusMessage update

--- AbstractBot/StatusMessage.cs
+++ AbstractBot/StatusMessage.cs
@@ -11,37 +11,47 @@ namespace AbstractBot;
 [PublicAPI]
 public class StatusMessage : IAsyncDisposable
 {
+    public static Task<StatusMessage> CreateAsync(BotBase bot, Chat chat, string text,
+        string postfix, bool? disableWebPagePreview = null, bool? protectContent = null, int? replyToMessageId = null,
+        bool? allowSendingWithoutReply = null, CancellationToken cancellationToken = default)
+    {
+        return CreateAsync(bot, chat, text, () => postfix, disableWebPagePreview, protectContent, replyToMessageId,
+            allowSendingWithoutReply, cancellationToken);
+    }
+
     public static async Task<StatusMessage> CreateAsync(BotBase bot, Chat chat, string text,
-        bool? disableWebPagePreview = null, bool? protectContent = null, int? replyToMessageId = null,
-        bool? allowSendingWithoutReply = null, string? postfix = null, CancellationToken cancellationToken = default)
+        Func<string>? postfixProvider = null, bool? disableWebPagePreview = null, bool? protectContent = null,
+        int? replyToMessageId = null, bool? allowSendingWithoutReply = null,
+        CancellationToken cancellationToken = default)
     {
         text = $"_{Utils.EscapeCharacters(text)}…_";
         Message message = await bot.SendTextMessageAsync(chat, text, null, ParseMode.MarkdownV2,
             disableWebPagePreview: disableWebPagePreview, disableNotification: true, protectContent: protectContent,
             replyToMessageId: replyToMessageId, allowSendingWithoutReply: allowSendingWithoutReply,
             cancellationToken: cancellationToken);
-        return new StatusMessage(bot, message, postfix, cancellationToken);
+        return new StatusMessage(bot, message, postfixProvider, cancellationToken);
     }
 
-    private StatusMessage(BotBase bot, Message message, string? postfix, CancellationToken cancellationToken)
+    private StatusMessage(BotBase bot, Message message, Func<string>? postfixProvider,
+        CancellationToken cancellationToken)
     {
         _bot = bot;
         _message = message;
-        _postfix = postfix;
+        _postfixProvider = postfixProvider;
         _cancellationToken = cancellationToken;
     }
 
-    public ValueTask DisposeAsync()
+    public async ValueTask DisposeAsync()
     {
         string text = _message.Text.GetValue(nameof(_message.Text));
-        text = $"_{Utils.EscapeCharacters(text)}_ Готово\\.{_postfix}";
-        Task<Message> task = _bot.EditMessageTextAsync(_message.Chat, _message.MessageId, text, ParseMode.MarkdownV2,
+        string? postfix = _postfixProvider?.Invoke();
+        text = $"_{Utils.EscapeCharacters(text)}_ Готово\\.{postfix}";
+        await _bot.EditMessageTextAsync(_message.Chat, _message.MessageId, text, ParseMode.MarkdownV2,
             cancellationToken: _cancellationToken);
-        return new ValueTask(task);
     }
 
     private readonly BotBase _bot;
     private readonly Message _message;
-    private readonly string? _postfix;
+    private readonly Func<string>? _postfixProvider;
     private readonly CancellationToken _cancellationToken;
 }
