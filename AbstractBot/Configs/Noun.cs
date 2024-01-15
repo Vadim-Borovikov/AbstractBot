16d25169f09d9af3a07665e1434f24cb2a74d5c8
Вадим Боровиков <vadim.borovikov@gmail.com>
2024-01-15 19:13:30 +0100

    Noun added

---
+++ AbstractBot/Configs/Noun.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GryphonUtilities.Helpers;
+using JetBrains.Annotations;
+
+// ReSharper disable NullableWarningSuppressionIsUsed
+
+namespace AbstractBot.Configs;
+
+[PublicAPI]
+public class Noun
+{
+    [Required]
+    [MinLength(1)]
+    public string Form1 { get; init; } = null!;
+    [Required]
+    [MinLength(1)]
+    public string Form24 { get; init; } = null!;
+    [Required]
+    [MinLength(1)]
+    public string FormAlot { get; init; } = null!;
+
+    public string FormatWithNumeric(string format, uint number)
+    {
+        return Text.FormatNumericWithNoun(format, number, Form1, Form24, FormAlot);
+    }
+}
